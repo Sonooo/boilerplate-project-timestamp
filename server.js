dfff33f4c343364cd4e50d3b80a169016169227e
saurabh sanap <35056406+Sonooo@users.noreply.github.com>
2022-01-02 10:57:02 +0530

    second

--- server.js
+++ server.js
@@ -24,28 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/", function (req, res) {
-  res.json({'unix': Date.now(), 'utc': Date()});
+app.get("/api", function (req, res) {
+  res.json({
+     unix: (new Date()).getTime(),
+     utc: (new Date()).toUTCString()
+
+  });
 });
 
-app.get("/api/timestamp/", function (req, res) {
-  res.json({'unix': Date.now(), 'utc': Date()});
-});
-
-app.get("/api/timestamp/:date", (req, res) => {
-  let dateString = req.params.date;
-
-  if (!isNaN(Date.parse(dateString))) {
-    let dateObject = new Date(dateString);
-    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
-  } else if (/\d{5,}/.test(dateString)) {
-      let dateInt = parseInt(dateString);
-      res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
-  } else {
-    res.json({ error: "Invalid Date" });
-  }
+app.get("/api/:date", function (req, res) {
+ const date = req.params.date;
+ console.log(date);
+ let unix, utc
+ if((new Date(parseInt(date))).toString()==="Invalid Date"){
+   res.json({error:"Invalid Date"});
+   return;
+ }
+ if(date.indexOf("-")===-1){
+   unix= parseInt(date);
+   utc= (new Date(parseInt(data))).toUTCString()
+ }
 
 });
+
+
 
 
 // listen for requests :)
